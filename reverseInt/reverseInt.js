eaba6155233ae24238ff897deb5fda70678c2634
Alexey <alexey.brenes@publicisgroupe.net>
2017-12-04 14:38:54 -0600

    Added Reverse Integer algorithm solution

---
+++ reverseInt/reverseInt.js
@@ -0,0 +1,13 @@
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n); // Math.sign returns +1 if positive or -1 if negative
+}
